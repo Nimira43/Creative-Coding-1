56adaf9772d3d136ede4ae83068870eee4c7cef8
Nick Rabone <n.rabone72@aol.com>
2024-06-26 21:22:44 +0100

    2nd phase of animation

--- 03---Animating-Lines/script.js
+++ 03---Animating-Lines/script.js
@@ -14,33 +14,40 @@ class Line {
         this.history = [{x: this.x, y: this.y}]
         this.lineWidth = Math.floor(Math.random() * 15 + 1)
         this.hue = Math.floor(Math.random() * 360)
+        this.maxLangth = 10
     }
     draw(context) {
         context.strokeStyle = 'hsl(' + this.hue + ', 100%, 50%)'
         context.lineWidth = this.lineWidth
         context.beginPath()
         context.moveTo(this.history[0].x, this.history[0].y)
-        for (let i = 0; i < 30; i++) {
-            this.x = Math.random() * this.canvas.width
-            this.y = Math.random() * this.canvas.height
-            this.history.push({x: this.x, y: this.y})
-        }
         for (let i = 0; i < this.history.length; i++) {
             context.lineTo(this.history[i].x, this.history[i].y)
         }
         context.stroke()
     }
+    update() {
+        this.x = Math.random() * this.canvas.width
+        this.y = Math.random() * this.canvas.height
+        this.history.push({ x: this.x, y: this.y })
+        if (this.history.length > this.maxLangth) {
+            this.history.shift()
+        }
+    }
 }
 
 const linesArray = []
-const numberOfLines = 1
+const numberOfLines = 2
 for (let i = 0; i < numberOfLines; i++) {
     linesArray.push(new Line(canvas))
 }
 
-
 function animate() {
-    linesArray.forEach(line => line.draw(ctx))
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    linesArray.forEach(line => {
+        line.draw(ctx)
+        line.update()
+    })
     requestAnimationFrame(animate)
 }
 
