8b3c927b37891abbf26522e26cb2c8c7684c5e31
Nick Rabone <n.rabone72@aol.com>
2024-06-26 20:55:37 +0100

    Updated code

--- Multi-Segmented-Lines/script.js
+++ Multi-Segmented-Lines/script.js
@@ -9,10 +9,9 @@ ctx.strokeStyle = 'magenta'
 class Line {
     constructor(canvas) {
         this.canvas = canvas
-        this.startX = Math.random() * this.canvas.width
-        this.startY = Math.random() * this.canvas.height
-        this.endX = Math.random() * this.canvas.width
-        this.endY = Math.random() * this.canvas.height
+        this.x = Math.random() * this.canvas.width
+        this.y = Math.random() * this.canvas.height
+        this.history = [{x: this.x, y: this.y}]
         this.lineWidth = Math.floor(Math.random() * 15 + 1)
         this.hue = Math.floor(Math.random() * 360)
     }
@@ -20,15 +19,22 @@ class Line {
         context.strokeStyle = 'hsl(' + this.hue + ', 100%, 50%)'
         context.lineWidth = this.lineWidth
         context.beginPath()
-        context.moveTo(this.startX, this.startY)
-        context.lineTo(this.endX, this.endY)
+        context.moveTo(this.history[0].x, this.history[0].y)
+        for (let i = 0; i < 3; i++) {
+            this.x = Math.random() * this.canvas.width
+            this.y = Math.random() * this.canvas.height
+            this.history.push({x: this.x, y: this.y})
+        }
+        for (let i = 0; i < this.history.length; i++) {
+            context.lineTo(this.history[i].x, this.history[i].y)
+        }
         context.stroke()
     }
 }
 
-const linesArray = []
-const numberOfLines = 50
-for (let i = 0; i < numberOfLines; i++) {
-    linesArray.push(new Line(canvas))
-}
-linesArray.forEach(line => line.draw(ctx))
+// const linesArray = []
+// const numberOfLines = 50
+// for (let i = 0; i < numberOfLines; i++) {
+//     linesArray.push(new Line(canvas))
+// }
+// linesArray.forEach(line => line.draw(ctx))
